a82f65ee8281ee6887d5168daf20288a6cf8763b
Mohamed Said <moasaid9@gmail.com>
2020-03-13 22:40:08 +0000

    com 2

--- scripts.js
+++ scripts.js
@@ -1,21 +1,58 @@
-var fulfilmentFee = { 
-    extraSmall: { weightKG: 5, pricePerKG: 10, fulfilledBy: '10 Days'},
-    small: { weightKG: 15, pricePerKG: 7.5, fulfilledBy: '10 Days'}, 
-    medium: { weightKG: 30, pricePerKG: 6, fulfilledBy: '2 Weeks'},
-    large: { weightKG: 50, pricePerKG: 3, fulfilledBy: '2 Weeks'}, 
-    extraLarge: { weightKG: 100, pricePerKG: 1.5, fulfilledBy: '20 Days'} 
-};
+var fulfilmentFee = [
+    { packageType: 'Extra Small', weightKG: 5, pricePerKG: 10, fulfilledBy: '10 Days' },
+    { packageType: 'Emall', weightKG: 15, pricePerKG: 7.5, fulfilledBy: '10 Days' },
+    { packageType: 'Medium', weightKG: 30, pricePerKG: 6, fulfilledBy: '2 Weeks' },
+    { packageType: 'Large', weightKG: 50, pricePerKG: 3, fulfilledBy: '2 Weeks' },
+    { packageType: 'Extra Large', weightKG: 100, pricePerKG: 1.5, fulfilledBy: '20 Days' }
+];
+
+var fulfilmentOptions = document.getElementById('package-size');
+const packList = fulfilmentFee.map(fulfilmentFee => (fulfilmentFee.packageType));
+
+for (fulFee in packList) {
+    fulfilmentOptions.options[fulfilmentOptions.options.length] = new Option(packList[fulFee], fulFee);
+}
+
+var forwardingFee = [
+    { country: 'United Arab Emirates', pricePerKG: 3 },
+    { country: 'China', pricePerKG: 3 },
+    { country: 'India', pricePerKG: 4 },
+    { country: 'Turkey', pricePerKG: 5 },
+    { country: 'Saudi Arabia', pricePerKG: 3 }
+];
 
 
-var forwardingFee = { 
-    UAE: { country: 'United Arab Emirates', pricePerKG: 3},
-    China: { country: 'China', pricePerKG: 3}, 
-    India: { country: 'India', pricePerKG: 4},
-    Turkey: { country: 'Turkey', pricePerKG: 5}, 
-    Saudi: { country: 'Saudi Arabia', pricePerKG: 3} 
-};
+var forwardingOptions = document.getElementById('package-location');
+const countryList = forwardingFee.map(forwardingFee => (forwardingFee.country));
 
-console.log(forwardingFee.UAE.pricePerKG);
+for (country in countryList) {
+    forwardingOptions.options[forwardingOptions.options.length] = new Option(countryList[country], country);
+}
 
 
 
+function getInput(e) {
+    const productPrice = document.getElementById('product-fee').value;
+    const forwardingOpt = document.getElementById('package-location').value;
+
+
+    // var forwarding;
+
+    // forwardingOpt.forEach(function(e){
+    //     forwarding = e.toString();
+    // });
+
+
+    const fulfilmentOpt = document.getElementById('package-size').value;
+    const print = document.getElementById('print');
+
+    if (!isNaN(productPrice)) {
+        print.innerHTML = `Your product cost £${productPrice} and size being ${fulfilmentOpt} from ${forwardingOpt}`
+    };
+
+    e.preventDefault();
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', getInput)
+console.log(fulfilmentOpt.toString());
